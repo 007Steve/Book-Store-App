26fd137bfb93e4ef4bbb67c15cdfc5db57b9646f
007steve <stephen.plummer1.sp@gmail.com>
2021-01-26 12:49:07 -0500

    first commit

---
+++ pages/index.js
@@ -0,0 +1,15 @@
+import Head from 'next/head'
+import styles from '../styles/Home.module.css'
+
+export default function Home() {
+  return (
+    <div className="">
+      <Head>
+        <title>Create Next App</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+     
+    </div>
+  )
+}
